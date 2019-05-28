3818d6a27469ec23db0bb81a5a18c89b1d55d459
Jannis Andrija Schnitzer <jannis.schnitzer@edg.ag>
2019-05-28 17:00:04 +0200

    Use arrayOffset out of a hunch

--- src/bazeltest/Main.java
+++ src/bazeltest/Main.java
@@ -5,6 +5,6 @@ import java.nio.ByteBuffer;
 public class Main {
 	public static void main(String[] args) {
 		ByteBuffer bb = ByteBuffer.allocate(132);
-		bb.put((byte) 'a');
+		System.out.println(bb.arrayOffset());
 	}
 }

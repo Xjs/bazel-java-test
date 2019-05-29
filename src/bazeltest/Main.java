582ed65a6dac93ed6063f9afec7d1d9446e8c8c3
Jannis Andrija Schnitzer <jannis.schnitzer@edg.ag>
2019-05-29 16:54:26 +0200

    Add CRLF test case

--- src/bazeltest/Main.java
+++ src/bazeltest/Main.java
@@ -1,10 +1,15 @@
 package bazeltest;
 
 import java.nio.ByteBuffer;
+import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		ByteBuffer bb = ByteBuffer.allocate(132);
-		System.out.println(bb.arrayOffset());
+		try {
+			ByteBufferWriter.write(ByteBuffer.allocate(42), System.out);
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+		}
+		System.out.println(BazelTest.compare(42));
 	}
 }
